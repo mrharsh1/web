035a842c46c1d91943b49724104e4a9daf6f81a1
HarshdevIt <mharshit1309@gmail.com>
2025-07-30 18:03:11 +0000

    update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -86,6 +86,19 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
         <main>{children}</main>
         {/* Footer */}
         <Footer />
+      
+<script type="text/javascript">
+var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+(function(){
+var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+s1.async=true;
+s1.src='https://embed.tawk.to/688a5e08d74a47192980ddb0/1j1e7ulbq';
+s1.charset='UTF-8';
+s1.setAttribute('crossorigin','*');
+s0.parentNode.insertBefore(s1,s0);
+})();
+</script>
+
       </body>
     </html>
   );
